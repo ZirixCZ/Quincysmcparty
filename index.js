ad45dadc8e6db71d9745a2e8daeb2853187b403d
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-10-10 11:25:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
-const faders = document.querySelectorAll(".fade-in");
-const options = {
-    threshold: 1,
-    rootMargin: "0px 0px -5px 0px"
-};
-const AppearOnScroll = new IntersectionObserver(function(entries,AppearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add("appear");
-            AppearOnScroll.unobserve(entry.target);
-        }
-    })
-}, options);
-
-faders.forEach(fader => {
-    AppearOnScroll.observe(fader);
-})
+function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-function textChange(){
-    document.getElementById('section-title_button-IP').innerHTML = "---";
-  }
-  
-  function ClickFuncsFeedback(){
-      textChange();
-  }
-
-
-
-  function ServerInvite() {
-    window.open("https://discord.gg/cVfkrEy")
-  }
+    function textChange(){
+        document.getElementById('title_p1-mserip').innerHTML = "---";
+      }
