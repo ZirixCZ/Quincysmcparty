1c61a55ef0ec351cf3529460c40b00c0bff3f2c3
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-10-31 10:33:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ function myFunction() {
       function LivemapOpenFUNC() {
         window.open("http://193.121.28.29:4269/")
       }
+
+      /*count down*/
+      var countDownDate = new Date ("Nov 7, 2020 15:00:00").getTime();
+      var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000*60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("count_down-p").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("count_down-p").innerHTML = "Enjoy!"
+        }
+      }, 1000);
