5caf47f30529fb4e80931e25c9f6f5533f1d6b69
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-12-03 20:15:05 +0100

    an IP update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function myFunction() {
         window.open("https://discord.gg/F2zFuaB")
       }
       function LivemapOpenFUNC() {
-        window.open("http://213.49.103.112:4269/")
+        window.open("http://62.235.120.101:4269/")
       }
 
       /*count down*/
