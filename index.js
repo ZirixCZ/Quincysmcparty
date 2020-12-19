b0fb9baaaac7953447ef935cc4fc45a751517856
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-12-19 21:13:43 +0100

    Updated the Dynmap IP
    Clients IP has been changed--

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function myFunction() {
         window.open("https://discord.gg/F2zFuaB")
       }
       function LivemapOpenFUNC() {
-        window.open("http://62.235.120.101:4269/")
+        window.open("http://83.134.97.93:4269/")
       }
 
       /*count down*/
