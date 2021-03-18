124d6c23218111d7812a40e997c24a5952963882
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-03-18 19:47:43 +0100

    rewritten the whole page with scss

--- index.js
+++ index.js
@@ -1,44 +1,115 @@
-function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/png" href="resources/quincyscpfavicon.png">
+    <script src="https://kit.fontawesome.com/463cbf7f77.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="main-parallax"></div>
+    <div class="navbar">
+        <a onclick="DiscordInvite()"><i class="fab fa-discord"></i> Discord</a>
+        <a onclick="LivemapOpenFUNC()"><i class="fas fa-map"></i> Live Map</a>
+        <a href="#community-s"><i class="fas fa-comments"></i> Community</a>
+    </div>
+    <div class="main-title">
+        <h1>Quincy's Minecraft Party</h1>
+    </div>
+    <div class="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
 
+</div>
+    <section id="info-s">
+        <div class="info-s-title">
+            <h2>WHY SHOULD YOU JOIN</h2>
+        </div>
+        <div class="info-s-li">
+            <lo>
+                <li>We have a cool interactive map</li>
+                <li>There is an active and welcoming community</li>
+                <li>We've got breath taking buildings</li>
+                <li>The server contains gorgeous game enhancing plugins</li>
+                <li>Respectful and active staff team</li>
+            </lo>
+        </div>
+        <div class="info-s-imagecointainer">
+            <img class="join-s-image" src="resources/Minecraftunderwater_2x.png">
+        </div>
+    </section>
+    <section id="fortressclose-s">
 
+    </section>
+    <section id="community-s">
+        <div class="community-s-title">
+            <h2>THE LIST OF COOL PEOPLE</h2>
+        </div>
+        <div class="community-s-li">
+            <lo>
+                <li><span id="community-s-li-firtplace1">D</span><span id="community-s-li-firtplace2">o</span><span id="community-s-li-firtplace3">g</span><span id="community-s-li-firtplace4">g</span><span id="community-s-li-firtplace5">o</span></li>
+                <li>Mik_F</li>
+                <li>S_VB</li>
+                <li>E Man</li>
+            </lo>
+        </div>
+        <div class="community-s-title1">
+            <h2>THE LIST OF STAFF</h2>
+        </div>
+        <div class="community-s-li1">
+            <lo>
+                <li>JustCallMeQuincy</li>
+                <li>S_VB</li>
+                <li>K3IN</li>
+            </lo>
+        </div>
+        <div class="community-s-imagecointainer">
+            <img class="community-s-image" src="resources/MinecraftGroupimage_2x.png">
+        </div>
+    </section>
+    <section id="fortressclose1-s">
 
-    function textChange(){
-        document.getElementById('title_p1-mserip').innerHTML = "---";
-      }
-
-      function VersionCheck() {
-        window.open("https://minecraft.gamepedia.com/Java_Edition_1.16.3")
-      }
-
-      function DiscordInvite() {
-        window.open("https://discord.gg/F2zFuaB")
-      }
-      function LivemapOpenFUNC() {
-        window.open("http://80.236.204.57:4269/")
-      }
-
-      /*count down
-      var countDownDate = new Date ("Nov 7, 2020 17:30:00").getTime();
-      var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000*60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        document.getElementById("title_p1-mserip").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("title_p1-mserip").innerHTML = "Enjoy!"
-        }
-      }, 1000);*/
+    </section>
+    <section id="coolplaces-s">
+        <div class="coolplaces-s-title">
+            <h2>COOL PLACES TO VISIT</h1>
+        </div>
+        <div class="coolplaces-s-list">
+            <h3>The Spawn</h3>
+            <p>A cool place to hang around</p>
+            <h4>Location: -215, 64, 215</h4>
+            <img class="coolplaces-s-image"src="resources/Minecraftspawn1.png">
+        </div>
+        <div class="coolplaces-s-list1">
+            <h3>Church</h3>
+            <p>Built by the server members</p>
+            <h4>Location: 400, 90, 670</h4>
+            <img class="coolplaces-s-image"src="resources/minecraftcastlebig_2x.png">
+        </div>
+        <div class="coolplaces-s-list2">
+            <h3>Hidden Base</h3>
+            <p>Built by E Man</p>
+            <h4>Location: try to find this on your journey!</h4>
+            <img class="coolplaces-s-image"src="resources/MinecraftEmansFort.png">
+        </div>
+        <div class="coolplaces-s-list3">
+            <h3>Big Farm</h3>
+            <p>Built by Quincy and Father</p>
+            <h4>Location: 2515, 69, 162</h4>
+            <img class="coolplaces-s-image"src="resources/Minecraftquincandfathersbase.png">
+        </div>
+        <div class="coolplaces-s-list4">
+            <h3>Camp in Vietnam</h3>
+            <p>Built by S_VB and Doggo</p>
+            <h4>Location: 3160, 63, 1773</h4>
+            <img class="coolplaces-s-image"src="resources/MinecraftVietnambase.png">
+        </div>
+    </section>
+    <script src="index.js"></script>
+</body>
+</html>
